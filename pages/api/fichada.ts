644ed3d0ddb17c833429378c4ee1472166e7432a
Ramiro Ferraresi <ramiro_ferraresi@hotmail.com>
2023-01-11 01:16:56 -0300

    ui fichadas

--- pages/api/fichada.ts
+++ pages/api/fichada.ts
@@ -26,18 +26,18 @@ export default async function fichada(
 
     users.forEach((element: any) => {
       let asis: any = atd.filter((x: any) => x.USERID === element.userid);
-      asis.sort((a:any,b:any)=>a.LOGID - b.LOGID)
+      asis.sort((a: any, b: any) => a.LOGID - b.LOGID)
       if (asis.length >= 1 && asis.find((x: any) => x.CHECKTYPE === "I")) {
         let Entrada = asis.find((x: any) => x.CHECKTYPE === "I")?.CHECKTIME;
         let Salida = asis.find((x: any) => x.CHECKTYPE === "O")?.CHECKTIME;
-        let diff:any = Salida? intervalToDuration({start: new Date(Entrada), end: new Date(Salida)}) : "-";
+        let diff: any = Salida ? intervalToDuration({ start: new Date(Entrada), end: new Date(Salida) }) : "-";
         let inf = {
-          id : element.userid,
+          id: element.userid,
           Nombre: element.name.toUpperCase(),
           Apellido: element.lastname.toUpperCase(),
-          Entrada: Entrada?  format(new Date(Entrada), "kk:mm") : "-", //(Entrada.getHours()<10?"0":"") + Entrada.getHours() + ":" + (Entrada.getMinutes()<10?"0":"") + Entrada.getMinutes(),  //(new Date(Entrada).getHours()<10?"0":"") +new Date(Entrada).getHours() + ":" + (new Date(Entrada).getMinutes() <10? "0": "") + new Date(Entrada).getMinutes(),
-          Salida: Salida? format(new Date(Salida), "kk:mm") : '-', //(Salida.getHours()<10? "0":"" ) + Salida.getHours()+ ":"+ (Salida.getMinutes()<10?"0":"") + Salida.getMinutes(),    //Salida && (new Date(Salida).getHours()<10?"0":"") +new Date(Salida).getHours() + ":" + (new Date(Salida).getMinutes() <10? "0": "") + new Date(Salida).getMinutes(),
-          Horas: diff === "-" ? "-" : ((diff.hours<10? "0":"") + diff.hours +":"+(diff.minutes<10? "0":"") + diff.minutes),
+          Entrada: Entrada ? format(new Date(Entrada), "kk:mm") : "-", //(Entrada.getHours()<10?"0":"") + Entrada.getHours() + ":" + (Entrada.getMinutes()<10?"0":"") + Entrada.getMinutes(),  //(new Date(Entrada).getHours()<10?"0":"") +new Date(Entrada).getHours() + ":" + (new Date(Entrada).getMinutes() <10? "0": "") + new Date(Entrada).getMinutes(),
+          Salida: Salida ? format(new Date(Salida), "kk:mm") : '-', //(Salida.getHours()<10? "0":"" ) + Salida.getHours()+ ":"+ (Salida.getMinutes()<10?"0":"") + Salida.getMinutes(),    //Salida && (new Date(Salida).getHours()<10?"0":"") +new Date(Salida).getHours() + ":" + (new Date(Salida).getMinutes() <10? "0": "") + new Date(Salida).getMinutes(),
+          Horas: diff === "-" ? "-" : ((diff.hours < 10 ? "0" : "") + diff.hours + ":" + (diff.minutes < 10 ? "0" : "") + diff.minutes),
         };
         reg.push(inf);
       }
